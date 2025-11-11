8daa2307c16ab07f26231d028b74a6991cd618a1
Jacob Densford <hello@jacobdensford.com>
2025-11-11 12:31:38 -0500

    Add raw sql

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { PrismaClient } = require('./generated/prisma')
+const { PrismaClient } = require('./generated/prisma');
+const { getUsersWithPosts } = require('./generated/prisma/sql');
 
 const prisma = new PrismaClient()
 
 async function main() {
+    // const userWithAddedPost = await prisma.post.create({
+    //     data: {
+    //         title: "First Post",
+    //         authorId: 1,
+    //     }
+    // })
     const allUsers = await prisma.user.findMany()
     console.log(allUsers)
+    const allPosts = await prisma.post.findMany();
+    console.log(allPosts);
+    const usersWithPostCount = await prisma.$queryRawTyped(getUsersWithPosts());
+    console.log(usersWithPostCount);
 }
 
 main()
